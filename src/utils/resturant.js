2b4cc631cb85e16d14265f8ce12ebb6ee780b739
Tumise <oladele.alade@gmail.com>
2020-11-10 07:54:33 +0100

    ft channged opeining hours

--- src/utils/resturant.js
+++ src/utils/resturant.js
@@ -20,7 +20,7 @@ export const addResturantToLocalStorage = (
 export const isOpen = () => {
   const closingTime = new Date();
   const openingTime = new Date();
-  openingTime.setHours(3, 0);
+  openingTime.setHours(10, 0);
   closingTime.setHours(20, 30);
   const currentTime = new Date();
   if (
