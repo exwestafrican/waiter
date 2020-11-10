9bffbac45be0adbbdf1eb34303c976b5dc959b04
Tumise <oladele.alade@gmail.com>
2020-11-10 08:26:45 +0100

    chore: replaced react logo with mobile waiter

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../logo.svg";
+import logo from "../../logo.PNG";
 import { path } from "../../url";
 import { Link } from "react-router-dom";
 import user from "../../auth";
